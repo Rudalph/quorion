743040b5fa1f88ead608497e7f23e8a95cc54ad0
iturhs20 <patilshruti7273@gmail.com>
2026-05-24 01:56:11 +0100

    backend call sorted

--- app/api/generate-keys/route.js
+++ app/api/generate-keys/route.js
@@ -94,14 +94,16 @@ export async function POST(req) {
       try {
         console.log("Calling PQC backend...");
 
-        const response = await fetch(
-          "http://localhost:8000/generate-keys",
+        const pythonApiUrl = process.env.PYTHON_API_URL || "http://localhost:8000";
+          const response = await fetch(
+            `${pythonApiUrl}/generate-keys`,
           {
             method: "POST",
             headers: {
               "Content-Type": "application/json",
             },
-            body: JSON.stringify({
+            body: JSON.stringify
+            ({
               algorithm: value,
             }),
           }
